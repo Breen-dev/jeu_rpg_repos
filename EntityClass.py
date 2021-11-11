dbf4f82d4c11bc13278793a689737ee7882047da
PurpleToti <94126798+PurpleToti@users.noreply.github.com>
2021-11-11 18:23:33 +0100

    Add files via upload

--- EntityClass.py
+++ EntityClass.py
@@ -3,8 +3,6 @@ class Entity():
 	def __init__(self, numberId, name):
 		self.numberId = int(numberId)
 		self.name = str(name)
-		self.healthPoints = int(healthPoints)
-		self.family = str(family)
 
 	#getters
 	def set_numberId(self, numberId):
@@ -17,31 +15,31 @@ class Entity():
 	def get_numberId(self):
 		return self.numberId
 
-	def get_name(self, name):
+	def get_name(self):
 		return self.name
 
 	#representation
 	def __repr__(self):
 		return str(self.numberId)
 
-class Caracter(Entity):
+class Character(Entity):
 	def __init__(self, numberId, name, health, attack):
-		super().__init__(numberId, name, health)
+		super().__init__(numberId, name)
 		self.attack = attack
 
 	#getters
 	def get_attack(self):
 		return self.attack
 
-	def set_health(self, health):
-		self.health = health
+	def get_health(self):
+		return self.health
 
 	#setters
 	def set_attack(self, attack):
 		self.attack = attack
 
-	def get_health(self):
-		return self.health
+	def set_health(self, health):
+		self.health = health
 
 	#classFunctions	
 	def isDead(self):
@@ -50,5 +48,36 @@ class Caracter(Entity):
 
 		return False
 
+class Human(Character):
+	def __init__(self, numberId, name, health, attack, items = []):
+		super().__init__(numberId, name, health, attack)
+		self.items = items
 
+	#getters
+	def get_items(self):
+		return self.items
 
+	#setters
+	def set_items(self, items):
+		self.items = items
+
+	#classFunction
+	def showMyItems(self):
+		finalList = self.get_name() + " own : "
+		for i in self.items:
+			finalList += i + ", "
+		print(finalList[:-2])
+
+	def doAFlip(self):
+		print(self.get_name() + " did a flip")
+
+class Monster(Character):
+	def __init__(self, numberId, name, health, attack, items = []):
+		super().__init__(numberId, name, health, attack)
+		self.items = items
+
+#def roulette(range, lot):
+#	res = randint(range[0], range[1])
+#	for i in lot:
+#		if res > i[0] and res < i[1]:
+#			event
